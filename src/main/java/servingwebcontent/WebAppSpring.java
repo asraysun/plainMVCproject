608fe343fa6c23d824689c4606ab5feb4aafca55
andrey zapolskiy <uniqo@yandex.by>
2020-10-11 01:16:43 +0300

    Initial commit

---
+++ src/main/java/servingwebcontent/WebAppSpring.java
@@ -0,0 +1,11 @@
+package servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpring {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppSpring.class, args);
+    }
+}
